64bac1b88a26fcdbe40ef506b6f51454519ffe3a
Ash Kamel <ajkamel@gmail.com>
2014-05-11 21:50:44 -0400

    update framework

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :signed_in?
+
+  private
+
+  def current_user
+    User.find_by(github_key: session[:github_key]) if session[:github_key]
+  end
+
+  def client
+    Octokit::Client.new(access_token: session[:github_key])
+  end
+
+  def signed_in?
+    redirect_to root_path if !current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'welcome#index'
+  resources :welcome
   resources :users
 
 end
